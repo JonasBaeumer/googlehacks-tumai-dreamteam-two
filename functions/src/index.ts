72e21bb36d0164a47485f8d61bd50b646cfc443f
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-09 02:17:41 +0200

    added mock endpoints for lovable frontend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,5 +12,12 @@ export {getDaily} from "./http/getDaily";
 export {getGithub} from "./http/getGithub";
 export {getXP} from "./http/getXP";
 
+// Demo API endpoints with mock data
+export {getSessionsDemo} from "./http/getSessionsDemo";
+export {getTopicsDemo} from "./http/getTopicsDemo";
+export {getDailyDemo} from "./http/getDailyDemo";
+export {getGithubDemo} from "./http/getGithubDemo";
+export {getXPDemo} from "./http/getXPDemo";
+
 // Session group analysis triggers
 export {onSessionGroupCreated, onSessionGroupUpdated} from "./triggers/sessionGroupAnalysis";
