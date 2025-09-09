af599f08d601d7213224e6f9128c851eaeb10812
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-09 02:10:16 +0200

    added new API and endpoints schemas for frontend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,12 @@ export {ingest} from "./http/ingest";
 export {claimCode} from "./http/claimCode";
 export {generateCode} from "./http/generateCode";
 
+// API endpoints for frontend
+export {getSessions} from "./http/getSessions";
+export {getTopics} from "./http/getTopics";
+export {getDaily} from "./http/getDaily";
+export {getGithub} from "./http/getGithub";
+export {getXP} from "./http/getXP";
+
 // Session group analysis triggers
 export {onSessionGroupCreated, onSessionGroupUpdated} from "./triggers/sessionGroupAnalysis";
